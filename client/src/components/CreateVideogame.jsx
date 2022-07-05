8333109855ae9793bac84cdbc41390970041522f
Federico Hugo Sebastian Dib <dibfede@gmail.com>
2022-07-05 13:45:06 -0300

    Update 05/07/2022

--- client/src/components/CreateVideogame.jsx
+++ client/src/components/CreateVideogame.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import {getGenres, getPlatforms, createVideogame} from '../redux/actions/index.js'
+import {getGenres, getPlatforms, createVideogame, getAllVideogames} from '../redux/actions/index.js'
 
 const FormStyled = styled.form`
     font-family: 'Press Start 2P', cursive;
@@ -36,7 +36,7 @@ const DivFormStyled = styled.div`
     justify-content: space-around;
     align-items: center;
     width: 100%;
-    margin-bottom: 20px;
+    margin-top: 20px;
     `
 
 const DivInputsStyled = styled.div`
@@ -48,7 +48,6 @@ const DivInputsStyled = styled.div`
     width: 100%;
     padding: 10px;
     border: 2px solid #111;
-    border-radius: 5px;
     color: #111;
     box-shadow: 3px 3px #111;
     align-items: left;
@@ -57,23 +56,38 @@ const DivInputsStyled = styled.div`
     &:focus {
       outline: none;
     }
+    &.error {
+      border: 2px solid hsl(0, 69%, 50%);
+      box-shadow: 3px 3px hsl(10, 95%, 21%);
+    }
+  }
+  textarea {
+    margin-bottom: 10px;
+    &.error {
+      border: 2px solid hsl(0, 69%, 50%);
+      box-shadow: 3px 3px hsl(10, 95%, 21%);
+    }
   }
   label {
     font-size: 0.85rem;
     width: 30%;
     text-align: left;
+    margin: 20px;
   }
   select {
     font-family: 'Press Start 2P', cursive;
     width: 80%;
-    margin: 10px;
+    margin: 0 20px 10px 20px;
     padding: 10px;
     border: 2px solid #111;
-    border-radius: 5px;
     color: #111;
     box-shadow: 3px 3px #111;
     &:focus {
       outline: none;
+    }
+    &.error {
+      border: 2px solid hsl(0, 69%, 50%);
+      box-shadow: 3px 3px hsl(10, 95%, 21%);
     }
   }
 `
@@ -115,7 +129,18 @@ const DivSelectButton = styled.div`
     width: 50%;
     flex-direction: column;
     align-items: center;
+    span{
+      text-align: center;
+    }
 `
+
+const SpanStyled = styled.span`
+  text-align: end;
+  padding: 0 10px;
+  width: 100%;
+  font-size: 0.6rem;
+  border-radius: 5px;
+  color: hsl(0, 69%, 50%);`
 
 export class CreateVideogame extends Component {
   constructor(props) {
@@ -130,9 +155,16 @@ export class CreateVideogame extends Component {
         platforms: [],
         genres: [],
       },
-      errors: []
+      errors: {
+        name: '',
+        description: '',
+        released: '',
+        rating: '',
+        image: '',
+        platforms: '',
+        genres: '',
+      }
     }
-    console.log(this.state.platforms)
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleGenres = this.handleGenres.bind(this)
@@ -144,52 +176,66 @@ export class CreateVideogame extends Component {
   componentDidMount() {
     this.props.getGenres()
     this.props.getPlatforms()
-    this.handleErrors()
-    console.log(this.state.errors)
+    // this.handleErrors()
   }
   
   handleChange(event) {
-    this.handleErrors()
-    console.log(this.state.errors)
     this.setState({
       newVideogame: {
         ...this.state.newVideogame,
-      [event.target.name]: event.target.value
+        [event.target.name]: event.target.value
       }
-   })
+    })
+    // this.handleErrors()
   }
 
   handleSubmit(e) {
     e.preventDefault()
-    
-    console.log(this.state.newVideogame)
-    this.props.createVideogame(this.state.newVideogame)
-  }
+    console.log('hola')
+
+    //verificar que las propiedades del state errors esten vacias
+
+    if (Object.values(this.state.errors).every(el => el === '') && Object.values(this.state.newVideogame).every(el => el !== '' && el !== 0 && el !== [])) {
+      this.props.getAllVideogames().then(() => {
+        console.log(this.props.videogames)
+        const videogame = this.props.videogames.find(videogame => videogame.name === this.state.newVideogame.name)
+        console.log(videogame)
+        if (videogame) {
+          alert('Videogame already exists')
+          this.props.history.push('/videogame/' + videogame.id)
+        } else {
+          this.props.createVideogame(this.state.newVideogame)
+          alert('Videogame created!')
+          this.props.history.push('/videogames')
+        }
+       }) 
+    } else {
+      alert('Please fill in all the fields')
+      }
+}
 
   handlePlatforms(event) {
-    this.handleErrors()
-    console.log(this.state.errors)
     if (!this.state.newVideogame.platforms.includes(event.target.value)) {
-    this.setState({
-      newVideogame: {
-        ...this.state.newVideogame,
-        platforms: [...this.state.newVideogame.platforms, event.target.value]
-      }
-    })
-  }
+      this.setState({
+        newVideogame: {
+          ...this.state.newVideogame,
+          platforms: [...this.state.newVideogame.platforms, event.target.value]
+        }
+      })
+    }
+    // this.handleErrors()
   }
 
   handleGenres(event) {
-    this.handleErrors()
-    console.log(this.state.errors)
     if (!this.state.newVideogame.genres.includes(event.target.value)) {
-    this.setState({
-      newVideogame: {
-        ...this.state.newVideogame,
-        genres: [...this.state.newVideogame.genres, event.target.value]
-      }
-    })
-  }
+      this.setState({
+        newVideogame: {
+          ...this.state.newVideogame,
+          genres: [...this.state.newVideogame.genres, event.target.value]
+        }
+      })
+    }
+    // this.handleErrors()
   }
 
   handleDelete(event) {
@@ -200,34 +246,80 @@ export class CreateVideogame extends Component {
         genres: this.state.newVideogame.genres.filter(genre => genre !== event.target.id)
       }
     })
+    // this.handleErrors()
   }
 
-  handleErrors() {
-    const errors = []
-    if (this.state.newVideogame.name === '') {
-      errors.push('Name is required')
+  handleErrors(e) {
+
+    if (!e){
+      return this.state.errors
     }
-    if (this.state.newVideogame.description === '') {
-      errors.push('Description is required')
+
+    const errors = {}
+
+    document.querySelectorAll('input').forEach(input => {
+      input.classList.remove('error')
+    })
+    document.querySelectorAll('textarea').forEach(textarea => {
+      textarea.classList.remove('error')
     }
-    if (this.state.newVideogame.released === '') {
-      errors.push('Released is required')
+    )
+    document.querySelectorAll('select').forEach(select => {
+      select.classList.remove('error')
     }
-    if (this.state.newVideogame.rating === '') {
-      errors.push('Rating is required')
+    )
+
+    if (!e.target.value) {
+      errors[e.target.name] = `${e.target.name} is required`
+      document.getElementById(`${e.target.id}`).classList.add('error')
+    } else (
+      errors[e.target.name] = ''
+    )
+
+    if (e.target.name === 'name' && (e.target.value && e.target.value.length < 3)) {
+      errors[e.target.name] = 'Name must be at least 3 characters'
+      document.getElementById(`${e.target.id}`).classList.add('error')
     }
-    if (this.state.newVideogame.image === '') {
-      errors.push('Image is required')
+
+    if (e.target.name === 'rating' && (e.target.value < 0 || e.target.value > 5)) {
+      errors[e.target.name] = 'Rating must be between 0 and 5'
+      document.getElementById(`${e.target.id}`).classList.add('error')
     }
-    if (this.state.newVideogame.platforms.length === 0) {
-      errors.push('Platform is required')
-    }
-    if (this.state.newVideogame.genres.length === 0) {
-      errors.push('Genre is required')
-    }
+    
+    // if (this.state.newVideogame.name === '') {
+    //   errors.name = 'Name is required'
+    //   document.getElementById('name').classList.add('error')
+    // }
+    // if (this.state.newVideogame.description === '') {
+    //   errors.description = 'description is required'
+    //   document.getElementById('description').classList.add('error')
+    // }
+    // if (this.state.newVideogame.released === '') {
+    //   errors.released = 'released is required'
+    //   document.getElementById('released').classList.add('error')
+    // }
+    // if (this.state.newVideogame.rating === '') {
+    //   errors.rating = 'rating is required'
+    //   document.getElementById('rating').classList.add('error')
+    // }
+    // if (this.state.newVideogame.image === '') {
+    //   errors.image = 'image is required'
+    //   document.getElementById('image').classList.add('error')
+    // }
+    // if (this.state.newVideogame.platforms.length === 0) {
+    //   errors.platforms = 'platforms is required'
+    //   document.getElementById('platforms').classList.add('error')
+    // }
+    // if (this.state.newVideogame.genres.length === 0) {
+    //   errors.genres = 'genres is required'
+    //   document.getElementById('genres').classList.add('error')
+    // }
+
     this.setState({
       errors: errors
     })
+    console.log(this.state.newVideogame)
+    console.log(this.state.errors)
   }
 
   render() {
@@ -242,28 +334,33 @@ export class CreateVideogame extends Component {
               <DivInputsStyled>
                 <DivStyled>
                   <label htmlFor="name">Name:</label>
-                  <input onChange={this.handleChange} type="text" id="name" name="name" />
+                  <input onFocus={(e) => this.handleErrors(e)} onBlur={(e) => this.handleErrors(e)} onChange={this.handleChange} type="text" id="name" name="name" placeholder="Name..."/>
                 </DivStyled>
+                  <SpanStyled>{this.state.errors.name && <p>{this.state.errors.name}</p>}</SpanStyled>
                 <DivStyled>
                   <label htmlFor="released">Released:</label>
-                  <input onChange={this.handleChange} type="text" id="released" name="released" />
+                  <input onFocus={(e) => this.handleErrors(e)} onBlur={(e) => this.handleErrors(e)} onChange={this.handleChange} type="text" id="released" name="released" placeholder="Date of release..."/>
                 </DivStyled>
+                <SpanStyled>{this.state.errors.released && <p>{this.state.errors.released}</p>}</SpanStyled>
                 <DivStyled>
                   <label htmlFor="rating">Rating:</label>
-                  <input onChange={this.handleChange} type="text" id="rating" name="rating" />
+                  <input onFocus={(e) => this.handleErrors(e)} onBlur={(e) => this.handleErrors(e)} onChange={this.handleChange} type="number" min="0" max="5" step="0.1" id="rating" name="rating" placeholder="Game rating (0-5)..."/>
                 </DivStyled>
+                <SpanStyled>{this.state.errors.rating && <p>{this.state.errors.rating}</p>}</SpanStyled>
                 <DivStyled>
                   <label htmlFor="image">Image:</label>
-                  <input onChange={this.handleChange} type="text" id="image" name="image" />
+                  <input onFocus={(e) => this.handleErrors(e)} onBlur={(e) => this.handleErrors(e)} onChange={this.handleChange} type="text" id="image" name="image" placeholder="Image link..."/>
                 </DivStyled>
+                <SpanStyled>{this.state.errors.image && <p>{this.state.errors.image}</p>}</SpanStyled>
                 <DivStyled>
                   <label htmlFor="description">Description:</label>
-                  <textarea rows='5' onChange={this.handleChange} id="description" name="description" />
+                  <textarea onFocus={(e) => this.handleErrors(e)} onBlur={(e) => this.handleErrors(e)} rows='5' onChange={this.handleChange} id="description" name="description" placeholder="Description..."/>
                 </DivStyled>
+                <SpanStyled>{this.state.errors.description && <p>{this.state.errors.description}</p>}</SpanStyled>
                 <DivGPStyled>
                 <label htmlFor="platforms">Platforms:</label>
-                  <select onChange={this.handlePlatforms} name="platforms">
-                    <option selected disabled value="Platform">Select Platform</option>
+                  <select id='platforms' onFocus={(e) => this.handleErrors(e)} onBlur={(e) => this.handleErrors(e)} onChange={this.handlePlatforms} name="platforms">
+                    <option id='optionPlatforms' selected hidden value="">Select Platform</option>
                     {this.props.platforms.map(platform => (
                       <option key={platform.id} value={platform.name}>{platform.name}</option>
                     ))}
@@ -276,12 +373,13 @@ export class CreateVideogame extends Component {
                       </DivSelectButton>
                     ))
                   }
+                  <SpanStyled>{this.state.errors.platforms && <p>{this.state.errors.platforms}</p>}</SpanStyled>
                 </DivGPStyled>
                 
                   <DivGPStyled>
                   <label htmlFor="genres">Genres:</label>
-                  <select onChange={this.handleGenres} name="genres">
-                    <option selected disabled value="">Select a genre</option>
+                  <select id='genres' onFocus={(e) => this.handleErrors(e)} onBlur={(e) => this.handleErrors(e)} onChange={this.handleGenres} name="genres">
+                    <option id='optionGenres' selected hidden value="">Select a genre</option>
                     {this.props.genres.map(genre => (
                       <option key={genre.id} value={genre.name}>{genre.name}</option>
                     ))}
@@ -295,6 +393,7 @@ export class CreateVideogame extends Component {
                       </DivSelectButton>
                     ))
                   }
+                  <SpanStyled>{this.state.errors.genres && <p>{this.state.errors.genres}</p>}</SpanStyled>
                   </DivGPStyled>
                   
                   
@@ -312,11 +411,13 @@ function mapStateToProps(state) {
   return {
       genres: state.genres,
       platforms: state.platforms,
+      videogames: state.videogames
     };
 }
 
 function mapDispatchToProps(dispatch) {
     return {
+      getAllVideogames: () => dispatch(getAllVideogames()),
       getPlatforms: () => dispatch(getPlatforms()),
       getGenres: () => dispatch(getGenres()),
       createVideogame: videogame => dispatch(createVideogame(videogame)),
